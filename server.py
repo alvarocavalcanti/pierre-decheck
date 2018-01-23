2b8f0874f45169cae1b6f5f6d4b18bd13b82f5b2
Alvaro Cavalcanti <alvarovictor@gmail.com>
2018-01-23 15:58:21 -0300

    Responding to a PR Comment Event

--- server.py
+++ server.py
@@ -1,28 +1,49 @@
-from flask import request, url_for
-from flask_api import FlaskAPI, status, exceptions
+import json
+
+import requests
+from flask import request
+from flask_api import FlaskAPI, status
 
 app = FlaskAPI(__name__)
+
+STATUS_ERROR = 'error'
+STATUS_FAILURE = 'failure'
+STATUS_PENDING = 'pending'
+STATUS_SUCCESS = 'success'
+BASE_GITHUB_URL = 'https://api.github.com/'
 
 
 @app.route("/", methods=['GET', 'POST'])
 def root_list():
-    """
-    List or create notes.
-    """
-    # if request.method == 'POST':
-    #     note = str(request.data.get('text', ''))
-    #     idx = max(notes.keys()) + 1
-    #     notes[idx] = note
-    #     return note_repr(idx), status.HTTP_201_CREATED
-
-    # request.method == 'GET'
-    # return [note_repr(idx) for idx in sorted(notes.keys())]
     return []
 
 
-@app.route("/watch/", methods=['POST'])
-def watch():
-  pass
+@app.route("/prcomment", methods=['POST'])
+def pull_request_comment():
+    owner = request.data.get('pull_request', {}).get('repo', {}).get('owner', {}).get('login', '')
+    repo = request.data.get('pull_request', {}).get('repo', {}).get('name', '')
+    sha = request.data.get('comment', {}).get('commit_id', '')
+    set_status(owner, repo, sha, STATUS_PENDING, "Checking dependencies...")
+    return {}, status.HTTP_201_CREATED
+
+
+def set_status(owner, repo, sha, commit_status, description=None):
+    url = '{}repos/{}/{}/statuses/{}'.format(
+        BASE_GITHUB_URL,
+        owner,
+        repo,
+        sha
+    )
+
+    payload = {
+        "state": commit_status,
+        "target_url": "foo",
+        "description": description,
+        "context": "continuous-integration/merge-watcher"
+    }
+
+    requests.request('POST', url, data=json.dumps(payload))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
