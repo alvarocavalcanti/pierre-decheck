b34356e653ef1705bafa19159edbae483207a511
Alvaro Cavalcanti <alvarovictor@gmail.com>
2018-02-21 10:22:05 -0300

    Extracting bodies from event payloads

--- tests.py
+++ tests.py
@@ -1,6 +1,9 @@
+import json
+
 from flask_testing import TestCase
 
 import server
+from payloads import PR_COMMENT_EVENT, PR_CREATED
 
 
 class ServerTest(TestCase):
@@ -20,3 +23,25 @@ class ServerTest(TestCase):
     def test_server_is_up(self):
         response = self.client.get("/")
         self.assert200(response)
+
+    def test_get_bodies_from_pr_comment_event(self):
+        bodies = server.get_bodies(json.loads(PR_COMMENT_EVENT))
+
+        self.assertEqual(2, len(bodies))
+        self.assertEqual("This is the PR body", bodies[0])
+        self.assertEqual("this is a comment", bodies[1])
+
+    def test_get_bodies_from_pr_created_event(self):
+        bodies = server.get_bodies(json.loads(PR_CREATED))
+
+        self.assertEqual(1, len(bodies))
+        self.assertEqual("This is the PR body", bodies[0])
+
+    def test_get_dependencies_identifiers_from_list(self):
+        bodies = ["Depends on #2", "", "depends on #3", "No dependencies here"]
+
+        dependencies = server.get_dependencies_from_bodies(bodies)
+
+        self.assertEqual(2, len(dependencies))
+        self.assertEqual("2", dependencies[0])
+        self.assertEqual("3", dependencies[1])
